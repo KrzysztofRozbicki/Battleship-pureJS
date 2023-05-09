64e6e73f028efb44b2f1e97534e92fb3cf1580a8
Krzysztof R <plkrozbicki@gmail.com>
2023-05-09 22:24:18 +0200

    Ships v 1.2 added animation Points and notifications

---
+++ src/assets/notify.js
@@ -0,0 +1,92 @@
+import Toastify from 'toastify-js';
+
+export const showHit = () =>
+  Toastify({
+    text: 'HIT!',
+    duration: 750,
+    destination: '',
+    newWindow: true,
+    close: false,
+    gravity: 'top', // `top` or `bottom`
+    position: 'center', // `left`, `center` or `right`
+    stopOnFocus: false, // Prevents dismissing of toast on hover
+    style: {
+      background: 'linear-gradient(to right, #00b09b, #96c93d)',
+    },
+    offset: {
+      x: 0, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+      y: '15vh', // vertical axis - can be a number or a string indicating unity. eg: '2em'
+    },
+  }).showToast();
+
+export const showSinked = (type, points) =>
+  Toastify({
+    text: `${type} SINKED! \n +${points} POINTS `,
+    duration: 1000,
+    destination: '',
+    newWindow: true,
+    close: false,
+    gravity: 'top', // `top` or `bottom`
+    position: 'center', // `left`, `center` or `right`
+    stopOnFocus: false, // Prevents dismissing of toast on hover
+    style: {
+      background: 'linear-gradient(to right, #00b09b, #96c93d)',
+      width: '20rem',
+      height: '30rem',
+      display: 'flex',
+      'justify-content': 'center',
+      'text-align': 'center',
+      'align-items': 'center',
+      'font-size': '3rem',
+    },
+    offset: {
+      x: '25vw', // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+      y: '15vh', // vertical axis - can be a number or a string indicating unity. eg: '2em'
+    },
+  }).showToast();
+
+export const showMiss = points =>
+  Toastify({
+    text: `MISS \n -${points} POINTS`,
+    duration: 750,
+    destination: '',
+    newWindow: true,
+    close: false,
+    gravity: 'top', // `top` or `bottom`
+    position: 'center', // `left`, `center` or `right`
+    stopOnFocus: false, // Prevents dismissing of toast on hover
+    style: {
+      background: 'linear-gradient(to right, #c78e43, #ff6600)',
+      'text-align': 'center',
+    },
+    offset: {
+      x: '25vw', // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+      y: '10vh', // vertical axis - can be a number or a string indicating unity. eg: '2em'
+    },
+  }).showToast();
+
+export const youWin = points =>
+  Toastify({
+    text: `YOU WIN AND GOT ${points} POINTS!!!`,
+    duration: 3000,
+    destination: '',
+    newWindow: true,
+    close: false,
+    gravity: 'top', // `top` or `bottom`
+    position: 'center', // `left`, `center` or `right`
+    stopOnFocus: false, // Prevents dismissing of toast on hover
+    style: {
+      background: 'linear-gradient(to right, #00b09b, #96c93d)',
+      width: '30rem',
+      height: '30rem',
+      display: 'flex',
+      'justify-content': 'center',
+      'text-align': 'center',
+      'align-items': 'center',
+      'font-size': '5rem',
+    },
+    offset: {
+      x: '25vw', // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+      y: '15vh', // vertical axis - can be a number or a string indicating unity. eg: '2em'
+    },
+  }).showToast();
--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
 import { Battleship } from './battleship';
+import { showHit, showSinked, showMiss, youWin } from './assets/notify';
 
 const SIZE = 10;
 const wrapperEl = document.getElementById('app');
 const startBtn = document.getElementById('start-btn');
-const missedShotsEl = document.getElementById('missed');
-const sinkedCorvetteEl = document.getElementById('corvette');
-const sinkedFregateEl = document.getElementById('fregate');
-const sinkedDestroyerEl = document.getElementById('destroyer');
-const sinkedCruiserEl = document.getElementById('cruiser');
+// const missedShotsEl = document.getElementById('missed');
+// const sinkedCorvetteEl = document.getElementById('corvette');
+// const sinkedFregateEl = document.getElementById('fregate');
+// const sinkedDestroyerEl = document.getElementById('destroyer');
+// const sinkedCruiserEl = document.getElementById('cruiser');
+const pointsEl = document.getElementById('points');
 
 const DESTROYED = 0;
 const MISS = 6;
 const EMPTY = 7;
+const CORVETTE_QTY = 4;
+const FREGATE_QTY = 3;
+const DESTROYER_QTY = 2;
+const CRUISER_QTY = 1;
 
+const POINTS_MISS = 10;
+const POINTS_CORVETTE = 20;
+const POINTS_FREGATE = 50;
+const POINTS_DESTROYER = 100;
+const POINTS_CRUISER = 200;
+
+let points = 0;
 let missedHits = 0;
 let sinkedCorvette = 0;
 let sinkedFregate = 0;
@@ -34,33 +47,52 @@ wrapperEl.addEventListener('click', event => {
   const y = +clickedCell.dataset.column;
   const ship = battleship.board[x][y];
   if (ship.size > DESTROYED && ship.size < MISS) {
-    clickedCell.innerHTML = '🔥';
+    clickedCell.innerHTML = '<img src="./burning.afd00d93.gif" class="animation" />';
+    if (ship.size > DESTROYED + 1) showHit();
     clickedCell.classList.add('hit');
     battleship.board[x][y].size -= 1;
     if (battleship.board[x][y].size === 0) {
       ship.cells.forEach(cell => {
         const destroyedCell = document.querySelector(`[data-number="${cell}"]`);
-        destroyedCell.innerHTML = '⚓';
+        destroyedCell.innerHTML =
+          '<img src="./sinking.d9fee310.gif" class="animation" loop="false"/>';
         destroyedCell.classList.add('destroyed');
       });
       console.log(ship.cells.length);
       switch (ship.cells.length) {
         case 1:
           sinkedCorvette += 1;
-          sinkedCorvetteEl.innerText = sinkedCorvette;
+          //   sinkedCorvetteEl.innerText = sinkedCorvette;
+          showSinked('Corvette', POINTS_CORVETTE);
+          points += POINTS_CORVETTE;
           break;
         case 2:
           sinkedFregate += 1;
-          sinkedFregateEl.innerText = sinkedFregate;
+          //   sinkedFregateEl.innerText = sinkedFregate;
+          showSinked('Fregate', POINTS_FREGATE);
+          points += POINTS_FREGATE;
           break;
         case 3:
           sinkedDestroyer += 1;
-          sinkedDestroyerEl.innerText = sinkedDestroyer;
+          //   sinkedDestroyerEl.innerText = sinkedDestroyer;
+          showSinked('Destroyer', POINTS_DESTROYER);
+          points += POINTS_DESTROYER;
           break;
         case 4:
           sinkedCruiser += 1;
-          sinkedCruiserEl.innerText = sinkedCruiser;
+          //   sinkedCruiserEl.innerText = sinkedCruiser;
+          showSinked('Cruiser', POINTS_CRUISER);
+          points += POINTS_CRUISER;
           break;
+      }
+      if (
+        sinkedCruiser === CRUISER_QTY &&
+        sinkedDestroyer === DESTROYER_QTY &&
+        sinkedFregate === FREGATE_QTY &&
+        sinkedCorvette === CORVETTE_QTY
+      ) {
+        console.log('YOU WIN!');
+        youWin(points);
       }
     }
   }
@@ -69,6 +101,9 @@ wrapperEl.addEventListener('click', event => {
     clickedCell.classList.add('miss');
     battleship.board[x][y] = MISS;
     missedHits += 1;
-    missedShotsEl.innerText = missedHits;
+    // missedShotsEl.innerText = missedHits;
+    showMiss(POINTS_MISS);
+    points -= POINTS_MISS;
   }
+  pointsEl.innerText = points;
 });
